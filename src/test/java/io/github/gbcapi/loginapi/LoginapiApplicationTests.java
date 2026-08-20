f6dd91636425841645ad7a8f0bd0e6fa1c1e0c87
gbcapi <gabrielcapatigbc@gmail.com>
2026-08-20 15:55:32 -0300

    chore: setup inicial do projeto Spring Boot com MySQL local

---
+++ src/test/java/io/github/gbcapi/loginapi/LoginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gbcapi.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
